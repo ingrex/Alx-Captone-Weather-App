f3a5bc21753677b8310c9fe9318061913d20d392
ingrex2 <thekingchinex123@gmail.com>
2025-10-16 17:28:08 +0100

    Forecast page fully created and designed

--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { FiArrowLeft } from "react-icons/fi";
-import weatherBg from "../assets/ClearNight.gif";
+import weatherBg from "../assets/ClearNight.jpg";
 
 
 
@@ -32,8 +32,69 @@ const Forecast = () => {
 
 
   return (
-    <div className="bg-cover min-h-screen text-whitemo" style={{ background: `url(${weatherBg})`}}>
-        
+    <div className=" bg-cover min-h-screen p-6 " style={{ backgroundImage: `url(${weatherBg})` }}>
+
+        <button onClick={() => navigate("/home")} className="text-white mb-1 fixed flex items-center gap-1 bg-black/25"><FiArrowLeft/> Home </button>
+
+        <div className="text-center "> 
+            <img src={`https://openweathermap.org/img/wn/${current.weather[0].icon}@4x.png`} alt="weather condition icon"  className="mx-auto mb-3 w-40 h-40 "/>
+
+            <h1 className="text-6xl text-white font-bold mb-2">{Math.round(current.main.temp)}°</h1>
+
+            <p className="text-xl capitalize text-white">{current.weather[0].description}</p>
+        </div>
+
+        {/*Hourly Forecast */}
+
+        <div className="mt-10">
+            <h2 className="text-lg font-semibold mb-3 text-white">Hourly Forecast</h2>
+
+            <div className="flex overflow-x-scroll space-x-4 pb-2 scrollbar-hide">{hours.map((hour, index) => (
+                
+                <div key={index} className="flex-shrink-0 bg-white/20 backdrop-blur-md p-4 rounded-2xl w-20 text-center text-white">
+
+                    <p className="text-sm">
+                        {index === 0 
+                        ? "Now" : new Date(hour.dt * 1000). getHours() + ":00"}
+                    </p>
+                    <img src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`}
+                alt="icon"
+                className="mx-auto w-10 h-10"/>
+
+                    <p className="font-semibold">{Math.round(hour.main.temp)}°</p>
+
+                </div>
+            ))}
+
+            </div>     
+        </div>
+
+        {/* 5 Days Forecaast */}
+        <div className="mt-10" >
+        <h2 className="text-lg text-white font-semibold mb-3" text-white>5-Days Forecast</h2>
+        <div className="space-y-3">
+          {days.map((day, index) => {
+            const date = new Date(day.dt * 1000);
+            const options = { weekday: "short" };
+            return (
+              <div
+                key={index}
+                className="bg-white/20 backdrop-blur-md p-3 rounded-2xl flex justify-between items-center text-white "
+              >
+                <div className="flex items-center gap-3">
+                  <img
+                    src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`}
+                    alt="icon"
+                    className="w-10 h-10"
+                  />
+                  <p>{date.toLocaleDateString("en-US", options)}</p>
+                </div>
+                <p className="font-semibold">{Math.round(day.main.temp)}°</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   )
 }
